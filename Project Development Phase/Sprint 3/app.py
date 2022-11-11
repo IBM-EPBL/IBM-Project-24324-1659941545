e7d2271ea7d374f18dd14ee0dbfbd1d6c1be92e1
Surya V <76275102+SuryaVelumani@users.noreply.github.com>
2022-11-11 20:50:22 +0530

    Update app.py

--- Project Development Phase/Sprint 3/app.py
+++ Project Development Phase/Sprint 3/app.py
@@ -2,20 +2,92 @@ from turtle import st
 from flask import Flask, render_template, request, redirect, url_for, session
 
 import ibm_db
-conn = ibm_db.connect("",'','')
-
+conn = 
 from flask_mail import Mail, Message
-   
+
+
+import ibm_boto3
+from ibm_botocore.client import Config, ClientError
+
+COS_ENDPOINT=
+COS_API_KEY_ID=
+COS_INSTANCE_CRN=
+
+
+# Create resource https://s3.ap.cloud-object-storage.appdomain.cloud
+cos = ibm_boto3.resource("s3",
+    ibm_api_key_id=COS_API_KEY_ID,
+    ibm_service_instance_id=COS_INSTANCE_CRN,
+    config=Config(signature_version="oauth"),
+    endpoint_url=COS_ENDPOINT
+)
+
 app = Flask(__name__)
-   
+
+def multi_part_upload(bucket_name, item_name, file_path):
+    try:
+        print("Starting file transfer for {0} to bucket: {1}\n".format(item_name, bucket_name))
+        # set 5 MB chunks
+        part_size = 1024 * 1024 * 5
+
+        # set threadhold to 15 MB
+        file_threshold = 1024 * 1024 * 15
+
+        # set the transfer threshold and chunk size
+        transfer_config = ibm_boto3.s3.transfer.TransferConfig(
+            multipart_threshold=file_threshold,
+            multipart_chunksize=part_size
+        )
+
+        # the upload_fileobj method will automatically execute a multi-part upload
+        # in 5 MB chunks for all files over 15 MB
+        with open(file_path, "rb") as file_data:
+            cos.Object(bucket_name, item_name).upload_fileobj(
+                Fileobj=file_data,
+                Config=transfer_config
+            )
+
+        print("Transfer for {0} Complete!\n".format(item_name))
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to complete multi-part upload: {0}".format(e))
+
+@app.route('/uploadResume', methods = ['GET', 'POST'])
+def upload():
+   if request.method == 'POST':
+       bucket='sv-demoibm1'
+       name_file = session['username']
+       name_file += '.png'
+       filenameis = request.files['file']
+       filepath = request.form['filepath']
+       f = filepath
+       f = f+filenameis.filename
+       print("----------------------",f)
+       multi_part_upload(bucket,name_file,f)
+       return redirect(url_for('dashboard'))
+       
+    
+   if request.method == 'GET':
+       return render_template('upload.html')
+
+
 app.secret_key = 'your secret key'
 mail = Mail(app) # instantiate the mail class
 
 # configuration of mail
+'''
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'suryaveducation@gmail.com'
+app.config['MAIL_PASSWORD'] = 'wulfxwjegogykktb'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+'''
 app.config['MAIL_SERVER']='smtp.sendgrid.net'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'apikey'
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_PASSWORD'] = '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'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
